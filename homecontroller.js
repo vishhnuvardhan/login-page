0ac78089c12fa208fb80d112763bc05a467179b1
vishhnuvardhan <59530501+vishhnuvardhan@users.noreply.github.com>
2020-01-24 11:14:04 +0530

    Delete homecontroller.js

--- homecontroller.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp');
- 
-  app.controller('HomeController', 
-  function($scope, $rootScope, $stateParams, $state, LoginService) {
-    $scope.user = $rootScope.userName;
-    
-  });
